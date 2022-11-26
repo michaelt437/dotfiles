33e74754f60cd85f3752e1d110d4ed1dd6e88824
Michael Tran <michaelt437@gmail.com>
2022-11-26 00:29:35 -0500

    fern and fugitive

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,6 +1,10 @@
 return require('packer').startup{
 	function(use)
 		use "morhetz/gruvbox"
+		-- For use with Fern, for now
+		use "antoinemadec/FixCursorHold.nvim"
+		use {"tpope/vim-fugitive", branch = "master"}
+		use {"lambdalisue/fern.vim", branch = "main"}
 
 	end
 }
