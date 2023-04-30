ee6b3eeee438426be96e73a3b8f2f9d035e9aa31
Michael Tran <michaelt437@gmail.com>
2023-04-30 11:41:57 -0400

    telescope, treesitter, fzy native

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -23,6 +23,19 @@ return require('packer').startup{
       requires = {"nvim-lua/plenary.nvim"}
     })
     use("MunifTanjim/prettier.nvim")
+    use ({
+      "nvim-treesitter/nvim-treesitter",
+      run = function()
+          local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+          ts_update()
+      end
+    })
+    use "nvim-telescope/telescope-fzy-native.nvim"
+    use({
+      "nvim-telescope/telescope.nvim",
+      tag = "0.1.1",
+      requires = {"nvim-lua/plenary.nvim"}
+    })
 
 		-- Themes
 		use "ayu-theme/ayu-vim"
